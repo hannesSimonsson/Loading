c3ab81d0c5a726693f8baf4d58de519838214cb9
Hannes <k.hannes.simonsson@gmail.com>
2020-11-19 21:46:12 +0100

    fixed layout and put timeout on presentation layout

--- app/src/main/java/com/example/loading/MainActivity.kt
+++ app/src/main/java/com/example/loading/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.loading
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 
+// Show presentation screen, web() called from presentation.xml
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.decorView.apply{systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN}
         setContentView(R.layout.presentation)
+
+        val intent = Intent(this, Browser::class.java)
+        Handler().postDelayed( {
+            startActivity(intent) }, 350
+        )
     }
 
     fun web(view: View) {
--- app/src/main/java/com/example/loading/browser.kt
+++ app/src/main/java/com/example/loading/browser.kt
@@ -1,11 +1,13 @@
 package com.example.loading
 
 import android.os.Bundle
+import android.view.ViewGroup
 import android.webkit.WebView
 import android.webkit.*
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.main_activity.*
 
+// Everything regarding web is handled here
 class Browser: AppCompatActivity() {
     private lateinit var webView: WebView
     private var currentURL = "https://www.loading.se"
@@ -14,6 +16,19 @@ class Browser: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
 
+        // Make status bar align with webview
+        val resourceId = resources.getIdentifier("status_bar_height", "dimen", "android")
+        val height : Int
+        height = if (resourceId > 0)
+            resources.getDimensionPixelSize(resourceId)
+        else
+            0
+
+        val param = swipe.layoutParams as ViewGroup.MarginLayoutParams
+        param.setMargins(0,height,0,0)
+        swipe.layoutParams = param
+
+        // Set up web client
         webView = findViewById(R.id.webview)
         webView.settings.setJavaScriptEnabled(true)
         webView.settings.setDomStorageEnabled(true);
@@ -31,11 +46,15 @@ class Browser: AppCompatActivity() {
     override fun onStart() {
         super.onStart()
 
+        // #fd2860 : Color
+        swipe.setColorSchemeColors(16590944)
+
+        // Pull down refresh
         swipe.setOnRefreshListener {
             currentURL = webView.url
             webView.loadUrl(currentURL)
             swipe.isRefreshing = false
-
         }
     }
 }
+
